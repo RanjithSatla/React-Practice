b137eab0992d48dfa0b667a4a1255826c228bcf7
RanjithSatla <ranjith.satla@vamstar.io>
2022-06-20 12:06:17 +0530

    Adding Styles Using Method

--- counter/src/Components/counter.jsx
+++ counter/src/Components/counter.jsx
@@ -2,19 +2,13 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
+    count: 1,
   };
 
-  styles = {
-    fontSize: 10,
-    fontWeight: "bold",
-  };
   render() {
     return (
       <div>
-        <span style={this.styles} className="badge badge-primary m-2">
-          {this.formatCount()}
-        </span>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
@@ -24,6 +18,12 @@ class Counter extends Component {
     const { count } = this.state;
     return count === 0 ? "Zero" : count;
   };
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
 }
 
 export default Counter;
