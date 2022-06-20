ab3fcaf438b6fdf284295ecd1845bdbc58e25046
RanjithSatla <ranjith.satla@vamstar.io>
2022-06-20 11:55:47 +0530

    Adding Styles

--- counter/src/Components/counter.jsx
+++ counter/src/Components/counter.jsx
@@ -3,14 +3,19 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    imgUrl: "https://picsum.photos/200",
+  };
+
+  styles = {
+    fontSize: 10,
+    fontWeight: "bold",
   };
   render() {
     return (
       <div>
-        <img src={this.state.imgUrl} />
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
+        <span style={this.styles} className="badge badge-primary m-2">
+          {this.formatCount()}
+        </span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
   }
