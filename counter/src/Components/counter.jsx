31512a134dd6f85eea221293460727a0bc92af1d
RanjithSatla <ranjith.satla@vamstar.io>
2022-06-21 11:04:06 +0530

    Passing Props

--- counter/src/Components/counter.jsx
+++ counter/src/Components/counter.jsx
@@ -2,14 +2,14 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
+    value: this.props.value,
   };
-
   handleIncrement = () => {
-    this.setState({ count: this.state.count + 1 });
+    this.setState({ value: this.state.value + 1 });
   };
-
   render() {
+    console.log(this.props);
+
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
@@ -23,12 +23,12 @@ class Counter extends Component {
     );
   }
   formatCount = () => {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value } = this.state;
+    return value === 0 ? "Zero" : value;
   };
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 }
