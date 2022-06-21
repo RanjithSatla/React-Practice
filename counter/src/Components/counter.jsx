1b2d2d15bd66e09e0dcf304b8f3f97bd1684440b
RanjithSatla <ranjith.satla@vamstar.io>
2022-06-21 10:55:24 +0530

    Composing Components

--- counter/src/Components/counter.jsx
+++ counter/src/Components/counter.jsx
@@ -2,30 +2,12 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 1,
-    // tags: ["tag1", "tag2", "tag3"],
+    count: 0,
   };
-
-  //   renderTags() {
-  //     if (this.state.tags.length === 0) return <p>There are no Tags!!!</p>;
-  //     return (
-  //       <ul>
-  //         {this.state.tags.map((tag) => (
-  //           <li key={tag}>{tag}</li>
-  //         ))}
-  //       </ul>
-  //     );
-  //   }
 
   handleIncrement = () => {
     this.setState({ count: this.state.count + 1 });
   };
-
-  //Passing Parameters
-
-  // doHandleIncrement = () => {
-  //   this.handleIncrement({ id: 1 });
-  // };
 
   render() {
     return (
