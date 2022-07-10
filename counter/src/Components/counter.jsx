5b239b8c63fb8f8158aee5bb7b50fb63df54633a
RanjithSatla <ranjith.satla@vamstar.io>
2022-07-10 12:51:07 +0530

    Handle Decrement

--- counter/src/Components/counter.jsx
+++ counter/src/Components/counter.jsx
@@ -2,10 +2,15 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    value: this.props.value,
+    /* state : Special property , its an obj that has data that he compenent needs */ value:
+      this.props.value,
   };
   handleIncrement = () => {
     this.setState({ value: this.state.value + 1 });
+  };
+
+  handleDecrement = () => {
+    this.setState({ value: 0 });
   };
   render() {
     console.log(this.props);
@@ -13,13 +18,19 @@ class Counter extends Component {
 
     return (
       <div>
-        {this.props.children}
+        {/* <h4>{this.props.id}</h4> */}
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
           onClick={() => this.handleIncrement({ id: 1 })}
           className="btn btn-secondary btn-sm"
         >
           Increment
+        </button>
+        <button
+          className="btn btn-danger btn-sm m-2"
+          onClick={() => this.props.onDelete(this.props.id)}
+        >
+          Delete
         </button>
       </div>
     );
